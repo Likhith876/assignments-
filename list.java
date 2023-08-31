6f7bd1f363d19ea18615166d7f396a4ca0935cda
Likhith M <142972330+Likhith876@users.noreply.github.com>
2023-08-31 16:14:43 +0530

    31 aug assignment

---
+++ list.java
@@ -0,0 +1,168 @@
+public class list {
+    public static void main(String[] args) {
+     
+String Perfume="PERFUME INFO";
+
+System.out.println(Perfume);
+
+   String perfumebrand="wild stone";
+   String perfumecolour="blue";
+   String perfumevolume="100ml";
+   String perfumescent="fresh";
+      int perfumecost=500;
+
+  System.out.println(perfumebrand);
+System.out.println(perfumecolour);
+System.out.println(perfumevolume);
+System.out.println(perfumescent);
+System.out.println(perfumecost);
+
+String creditcard="CREDITCARD INFO";
+System.out.println(creditcard);
+
+    String  creditcardbrand="Hdfc";
+    long  creditcardlimit=100000;
+    int creditcardrepaydate=5;
+      String creditcardinterestrate="10%";
+     String creditcardexpiry="02/2026";
+
+System.out.println(creditcardbrand);
+System.out.println(creditcardlimit);
+System.out.println(creditcardrepaydate);
+System.out.println(creditcardinterestrate);
+System.out.println(creditcardexpiry);
+
+String bulb="BULB INFO";
+System.out.println(bulb);
+
+String bulbbrand="Havells";
+String bulbcolour="White";
+String bulbtype="LED";
+double bulbprice=299.99;
+boolean isitwaterproof=true;
+
+System.out.println(bulbbrand);
+System.out.println(bulbcolour);
+System.out.println(bulbtype);
+System.out.println(bulbprice);
+System.out.println(isitwaterproof);
+
+String apartment="APARTMENT INFO";
+System.out.println(apartment);
+
+
+String apartmentname="Alpha Towers";
+String apartmentfloor="10th floor";
+int apartmentparkings=2;
+long apartmentcost=50000000;
+String apartmentspecs="3bhk";
+
+System.out.println(apartmentname);
+System.out.println(apartmentfloor);
+System.out.println(apartmentparkings);
+System.out.println(apartmentcost);
+System.out.println(apartmentspecs);
+
+String earphones="EARPHONE INFO";
+System.out.println(earphones);
+
+
+String earphonebrand="oneplus";
+int earphoneprice=5000;
+String earphonecolour="Black";
+String earphonelatency="103 ms";
+String earphoneconectivity="bluetooth";
+
+System.out.println(earphonebrand);
+System.out.println(earphoneprice);
+System.out.println(earphonecolour);
+System.out.println(earphonelatency);
+System.out.println(earphoneconectivity);
+
+String bag="BAG INFO";
+System.out.println(bag);
+
+String bagbrand="Safari";
+String bagcolour="Red";
+double bagprice=4999.88;
+String bagtype="laptopbag";
+int bagcompartments=4;
+
+System.out.println(bagbrand);
+System.out.println(bagcolour);
+System.out.println(bagprice);
+System.out.println(bagtype);
+System.out.println(bagcompartments);
+
+String washingmachine="WASHING MACHINE INFO";
+System.out.println(washingmachine);
+
+String washingmachinebrand="samsung";
+String washingmachinetype="Topload";
+String washingmachinecolour="cian";
+long washingmachineprice=20000;
+String washingmachineload="7kg";
+
+System.out.println(washingmachinebrand);
+System.out.println(washingmachinetype);
+System.out.println(washingmachinecolour);
+System.out.println(washingmachineprice);
+System.out.println(washingmachineload);
+
+String powerbank="POWERBANK INFO";
+System.out.println(powerbank);
+
+String powerbankbrand="Lenevo";
+String powerbankcolour="balck";
+String powerbankcapacity="20000mah";
+int powerbankprice=999;
+int powerbankports=3;
+boolean fastchargingsupport=true;
+
+System.out.println(powerbankbrand);
+System.out.println(powerbankcolour);
+System.out.println(powerbankcapacity);
+System.out.println(powerbankprice);
+System.out.println(powerbankports);
+System.out.println(fastchargingsupport);
+
+String Watch="WATCH INFO";
+System.out.println(Watch);
+
+String watchbrand="Titan";
+long watchprice=31000;
+String watchcolour="ocean blue";
+String watchtype="analogue";
+String watchsize="slim";
+
+System.out.println(watchbrand);
+System.out.println(watchprice);
+System.out.println(watchcolour);
+System.out.println(watchtype);
+System.out.println(watchsize);
+
+String mobile="MOBILE INFO";
+System.out.println(mobile);
+
+String mobilebrand="Realme";
+String mobilemodel="11pro";
+int mobileprice=26000;
+String mobileOS="Android";
+String mobilebattery="5000mah";
+
+System.out.println(mobilebrand);
+System.out.println(mobilemodel);
+System.out.println(mobileprice);
+System.out.println(mobileOS);
+System.out.println(mobilebattery);
+
+
+
+
+
+
+
+     
+
+    }
+}
